91da4a1acf9a94e6b3e5a269cd42b31f0ae99089
Aug749 <aug749@gmail.com>
2015-02-28 16:07:04 -0500

    Added .lua script

---
+++ Hackathon/test.lua
@@ -0,0 +1,49 @@
+scriptId = 'com.thalmic.examples.myfirstscript'
+scriptTitle = "My First Script"
+scriptDetailsUrl = ""
+
+myo.setLockingPolicy("standard")
+
+function onForegroundWindowChange(app, title)
+	myo.debug("onForegroundWindowChange: " .. app .. ", " .. title)
+    return true
+end
+
+function onPoseEdge(pose, edge)
+	
+	if(edge == "on") then
+		if (pose == "fist") then
+			Falcon()
+		--elseif (pose == "fist" and ...) then
+		--	Punch()
+		elseif (pose == "fingersSpread") then
+			Yes()
+		elseif (pose == "waveIn") then
+			ComeOn()
+		end
+	end
+end
+ 
+function Falcon()
+	myo.debug("1")
+	myo.vibrate("short")	
+	myo.keyboard("1","press")
+end
+
+function Punch()
+	myo.debug("2")
+	myo.vibrate("short")
+	myo.keyboard("2","down")
+end
+
+function Yes()
+	myo.debug("3")
+	myo.vibrate("short")
+	myo.keyboard("3","down")
+end
+
+function ComeOn()
+	myo.debug("4")
+	myo.vibrate("short")
+	myo.keyboard("4","down")
+end
